e2548076ff26cb30193d40c1ee617365c33d7c26
Álvaro Ferreira <alvaro.ferreira@amcom.com.br>
2023-03-14 11:30:21 -0300

    fix: image method

--- src/services/image.ts
+++ src/services/image.ts
@@ -3,15 +3,14 @@ export const getImageURLByPhrase = async (phrase: string): Promise<string> => {
     try {
         const response = await fetch(`https://pixabay.com/api/?key=${apiKey}&q=${phrase}&orientation=horizontal`);
         const data = await response.json();
-        if (!data) throw ''
+        if (!data || !data.hits || data.hits.length <= 0) throw ''
         const sel = Math.floor(Math.random() * (data.hits.length - 0 + 1) + 0)
-        if (!sel || !data.hits) throw ''
         const randomImageURL = data.hits[sel].webformatURL;
-        if (!randomImageURL) throw ''
         return randomImageURL + ''
     } catch (Exception) {
         const response = await fetch(`https://pixabay.com/api/?key=${apiKey}&q=study&orientation=horizontal`);
         const data = await response.json();
+        if (!data || !data.hits || data.hits.length <= 0) return 'https://cdn.pixabay.com/photo/2020/05/13/13/32/video-conference-5167472_960_720.jpg'
         const sel = Math.floor(Math.random() * (data.hits.length - 0 + 1) + 0)
         const randomImageURL = data.hits[sel].webformatURL;
         return randomImageURL + ''
